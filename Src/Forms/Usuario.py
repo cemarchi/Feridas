fa86e67114e93b83ea045b3552eb757263b1958b
cemarchi <carlos_marchi@yahoo.com.br>
2016-12-08 02:02:50 -0200

    Estrutura

--- Src/Forms/Usuario.py
+++ Src/Forms/Usuario.py
@@ -1,13 +1,14 @@
 from flask_wtf import Form
-from wtforms import StringField, PasswordField, validators
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Email, Length
 
 
 class Usuario(Form):
-    senha = PasswordField('senha', validators=[validators.required(message='senha é requerida'),
-                                               validators.length(min=4, max=10, message='senha deve possuir entre 4 a 10 caracteres')])
-    email = StringField('email', validators=[validators.required(message='e-mail é requerido'),
-                                             validators.length(min=3, max=20, message='email deve possuir entre 3 a 20 caracteres'),
-                                             validators.email(message='e-mail inválido')])
-    nome = StringField('nome', validators=[validators.required(message='nome é requerido'),
-                                           validators.length(min=3, max=50, message='nome deve possuir entre 3 a 50 caracteres')])
+    senha = PasswordField('senha', validators=[DataRequired(message='Senha é requerida.'),
+                                               Length(min=4, max=10, message='Senha deve possuir entre 4 a 10 caracteres.')])
+    email = StringField('email', validators=[DataRequired(message='E-mail é requerido.'),
+                                             Length(min=3, max=20, message='E-mail deve possuir entre 3 a 20 caracteres.'),
+                                             Email(message='E-mail inválido.')])
+    nome = StringField('nome', validators=[DataRequired(message='Nome é requerido.'),
+                                           Length(min=3, max=50, message='Nome deve possuir entre 3 a 50 caracteres.')])
 
---
+++ Src/models/Autenticacao/Usuario.py
@@ -0,0 +1,35 @@
+from flask_security import UserMixin
+from flask.ext import bcrypt
+from datetime import datetime
+import json
+
+from Src import db
+
+
+class Usuario(db.Model, UserMixin):
+    __tablename__ = 'usuario'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(20), unique=True, nullable=False)
+    senha = db.Column(db.String(10), nullable=False)
+    ativo = db.Column(db.Boolean, nullable=False)
+    autenticado = db.Column(db.Boolean, nullable=False)
+    confirmado_em = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, nome, email, senha, ativo=True, autenticado=True):
+        self.nome = nome
+        self.email = email
+        self.senha = bcrypt.generate_password_hash(senha)
+        self.ativo = ativo
+        self.confirmado_em = datetime.utcnow()
+        self.autenticado = autenticado
+
+    def is_active(self):
+        return self.ativo
+
+    def senha_correta(self, senha_texto):
+        return bcrypt.check_password_hash(self.senha, senha_texto)
+
+    def to_json(self):
+        return dict(nome=self.nome, email=self.email)
